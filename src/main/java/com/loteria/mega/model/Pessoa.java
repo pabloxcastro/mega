3173e87911a05693abce34e8d98853fd73149318
pabloxcastro <pablo.castro@hotmail.com>
2021-05-14 22:52:49 -0300

    Email inválido

--- src/main/java/com/loteria/mega/model/Pessoa.java
+++ src/main/java/com/loteria/mega/model/Pessoa.java
@@ -3,12 +3,14 @@ package com.loteria.mega.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Pessoa {
 
 	@Id
 	@Email
+	@NotBlank
 	private String email;
 
 	public Pessoa(String email) {
